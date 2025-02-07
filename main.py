01c7eedad10b3f5914442440fcfef988cdd0c5e8
rsradev <raddy.radev@gmail.com>
2025-02-07 09:58:41 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import socket
+
+app = FastAPI()
+@app.get("/")
+
+
+def hello():
+    return "Hello World"
+
+
+@app.get("/hostname")
+def get_hostname():
+    return {"hostname": socket.gethostname()}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
